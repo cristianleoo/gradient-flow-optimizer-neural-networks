19e0fbb8e6c911e21b1121853627e7169b779e1c
Cristian Leo <cristianleo120@gmail.com>
2024-09-28 17:24:37 -0400

    source code

--- src/gradient_visualizer/training.py
+++ src/gradient_visualizer/training.py
@@ -1,29 +1,24 @@
 import torch
+from .model import BaseModel
+from .types import NetworkConfig
 
-def train_model(model, optimizer, loss_fn, num_epochs, X, y, progress_bar, status_text):
+def train_model(model: BaseModel, optimizer, loss_fn, config: NetworkConfig, X, y, progress_callback):
     loss_history = []
     gradient_flow = []
 
-    for epoch in range(num_epochs):
+    for epoch in range(config.num_epochs):
         optimizer.zero_grad()
         outputs = model(X)
         loss = loss_fn(outputs, y)
         loss.backward()
 
-        # Capture gradients
-        grads = []
-        for param in model.parameters():
-            if param.grad is not None:
-                grads.append(param.grad.norm().item())
+        grads = [param.grad.norm().item() for param in model.parameters() if param.grad is not None]
         gradient_flow.append(grads)
 
         optimizer.step()
-
-        # Record loss
         loss_history.append(loss.item())
 
-        # Update progress bar
-        progress_bar.progress((epoch + 1) / num_epochs)
-        status_text.text(f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item():.4f}")
-    
+        # Call the progress callback to update Streamlit
+        progress_callback(epoch, loss.item())
+
     return loss_history, gradient_flow
